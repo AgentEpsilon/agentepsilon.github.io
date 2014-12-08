eb0cc18d27ee52ea0f1487211949885ca3722db0
Evan Miller <miller.evan815@gmail.com>
2014-12-08 10:29:32 -0500

    Moved trianglify to main page

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-var t = new Trianglify();
-var pattern = t.generate(window.innerWidth, window.innerHeight);
-
 var frames = ["<h1><a href='Physics/Physics.html'>Physics</a></h1><h3>A Unity Physics Simulation</h3><iframe width='100%' height='100%' src='Physics/Physics.html' frameborder='0' />",
 "<h1><a href='//www.github.com/AgentEpsilon/GradeCalc'>GradeCalc</a></h1><h3>A BPS Grade Calculator</h3><iframe width='100%' height='100%' src='GradeCalc/index.html' frameBorder='0'></iframe>",
 "<h1><a href='//www.github.com/AgentEpsilon/Mineshock'>Mineshock</a></h1><h2>A Minecraft mod in progress</h2>"];
@@ -45,7 +42,6 @@ var slideLeft = function(){
 
 
 $(document).ready(function(){
-$("body").attr('style', 'background-image: '+pattern.dataUrl);
 $(".center").append("<div class='frame'>"+frames[0]+"</div>");
 console.log("Page Loaded.");
 });
